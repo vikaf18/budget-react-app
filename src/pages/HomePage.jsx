df4c38fdee20ae48057cf81a67248decb527cde1
vika <viktoriafedorova18072003@gmail.com>
2023-01-21 10:58:52 +0300

    Add style ang format function

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,52 @@
+// INCOME - доходы
+// EXPENCE -расходы
+
+const OPERATION_TYPES = {
+    INCOME: "income",
+    EXPENSE: "expense"
+};
+
+const OPERATIONS = [
+    {
+        id: 1,
+        category: "products",
+        value: 3000,
+        type: "expense",
+        date: new Date()
+    },
+    {
+        id: 2,
+        category: "salary",
+        value: 50315,
+        type: "income",
+        date: new Date()
+    },
+    {
+        id: 3,
+        category: "car",
+        value: 20000,
+        type: "expense",
+        date: new Date()
+    }
+];
+
+// Форматирование чисел
+const formatNumber = (value) => {
+    return Intl.NumberFormat('ru-RU').format(parseInt(value));
+}
+
+// Функция форматирования для денег
+const formatMoney = (value) => {
+    return `${formatNumber(value)} руб.`;
+}
+
 const HomePage = () => {
     return (
         <section>
             <div className="container">
                 <div className="balance">
                     <h2>
-                        30 000 руб.
+                        {formatMoney(50275)}
                     </h2>
                 </div>
 
@@ -14,7 +56,7 @@ const HomePage = () => {
 
                         <div className="wrapper">
                             <input 
-                                type="number" 
+                                type="text" 
                                 name="balance" 
                                 placeholder="30 000" 
                             />
@@ -34,17 +76,31 @@ const HomePage = () => {
                     </h2>
 
                     <div className="filter">
-                        <button>Все операции</button>
-                        <button>Все доходы</button>
-                        <button>Все расходы</button>
+                        <button className="button sm">Все операции</button>
+                        <button className="button sm green">Все доходы</button>
+                        <button className="button sm red">Все расходы</button>
                     </div>
 
                     <div className="operations">
-                        <div className="operation">
-                            <h4 className="total">30 000 руб.</h4>
-                            <p className="category">Категория: автомобиль</p>
-                            <button className="button button--remove">Удалить</button>
-                        </div>
+                        {
+                            OPERATIONS.map((operation) => {
+                                return (
+                                    <div key={operation.id} className="operation">
+                                        <div className={`circle ${operation.type === OPERATION_TYPES.INCOME ? "income" : "expense"}`}>
+                                            {
+                                                operation.type === OPERATION_TYPES.INCOME ? 
+                                                <i className="fa-solid fa-money-bill"></i>
+                                                : 
+                                                <i className="fa-solid fa-shop"></i>
+                                            }
+                                        </div>
+                                        <p className="category">Категория: {operation.category}</p>
+                                        <h4 className="total">{formatMoney(operation.value)}</h4>
+                                        <button className="button button--remove">Удалить</button>
+                                    </div>
+                                );
+                            })
+                        }
                     </div>
 
                     <div className="pagination">
